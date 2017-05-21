726240aa7133090c0287fb07f4daaf2284b0d9c0
jeneser <jeneserwang@gmail.com>
2017-05-21 12:28:22 +0800

    Adjust home layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { CollectionComponent } from '../../components/collection/collection.component';
 
 @Component({
   selector: 'app-home',
