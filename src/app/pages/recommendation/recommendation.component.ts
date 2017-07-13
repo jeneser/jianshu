5f07625be97b1a39722f1d8e6d3736c4c00927ff
jeneser <jeneserwang@gmail.com>
2017-07-13 19:16:38 +0800

    Add some commponents and elements to recommendation page

--- src/app/pages/recommendation/recommendation.component.ts
+++ src/app/pages/recommendation/recommendation.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ThumbnailComponent } from '../../components/thumbnail/thumbnail.component';
+import { PlaceholderComponent } from '../../components/placeholder/placeholder.component';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
+
 @Component({
   selector: 'app-recommendation',
   templateUrl: './recommendation.component.html',
--- src/app/pages/recommendation/recommendation.module.ts
+++ src/app/pages/recommendation/recommendation.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { RecommendationRoutingModule } from './recommendation-routing.module';
 import { RecommendationComponent } from './recommendation.component';
+import { ThumbnailComponent } from '../../components/thumbnail/thumbnail.component';
+import { PlaceholderComponent } from '../../components/placeholder/placeholder.component';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RecommendationRoutingModule
   ],
-  declarations: [RecommendationComponent]
+  declarations: [
+    RecommendationComponent,
+    ThumbnailComponent,
+    PlaceholderComponent,
+    AvatarComponent
+  ]
 })
 export class RecommendationModule { }
